3be0f080eb075711e1ebd41cec8318049113b5e8
1E10 Laptop <sample.bths@edu>
2022-10-06 11:01:23 -0400

    dw

---
+++ src/TipCalculator.java
@@ -0,0 +1,56 @@
+public class TipCalculator {
+
+
+    // instance variables
+
+    private int numPeople;
+    private int tipPercentage;
+    private double totalBillBeforeTip;
+
+
+    // constructors
+
+    public TipCalculator(int numPeople, int tipPercentage) {
+        this.numPeople = numPeople;
+        this.tipPercentage = tipPercentage;
+        totalBillBeforeTip = 0.0; // default value
+    }
+
+
+    // methods
+
+    public double getTotalBillBeforeTip() {
+        return totalBillBeforeTip;
+    }
+
+    public int getTipPercentage() {
+        return tipPercentage;
+    }
+
+    public void addMeal(double cost) {
+        totalBillBeforeTip += cost;
+        if (cost == -1) {
+            totalBillBeforeTip++;
+        }
+    }
+
+    public double tipAmount() {
+        return ((tipPercentage / 100) * totalBillBeforeTip);
+    }
+
+    public double totalBill() {
+        return tipAmount() + getTotalBillBeforeTip();
+    }
+
+    public double perPersonCostBeforeTip() {
+        return getTotalBillBeforeTip() / numPeople;
+    }
+
+    public double perPersonTipAmount() {
+        return tipAmount() / numPeople;
+    }
+
+    public double perPersonTotalCost() {
+        return totalBill() / numPeople;
+    }
+}
---
+++ src/TipCalculatorRunner.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
+public class TipCalculatorRunner {
+    public static void main(String[] args) {
+
+        // variables
+        int numPeople = 0;
+        int tipPercentage = 0;
+        double dishCost = 0.0;
+
+        // import objects
+        Scanner scan = new Scanner(System.in);
+        DecimalFormat formatter = new DecimalFormat("#.##");
+        TipCalculator calc = new TipCalculator(numPeople, tipPercentage);
+
+        // user input
+        System.out.println("Welcome to the tip calculator!");
+        System.out.print("Enter the amount of people in your group: ");
+        numPeople = scan.nextInt();
+        scan.nextLine();
+        System.out.print("Enter the tip percentage (0-100): ");
+        tipPercentage = scan.nextInt();
+        scan.nextLine();
+        while (dishCost != -1) {
+            System.out.print("Enter a cost in dollars and cents (-1 to end): ");
+            dishCost = scan.nextDouble();
+            calc.addMeal(dishCost);
+            scan.nextLine();
+        }
+
+        // calculations
+        System.out.println("Total bill before tip: " + formatter.format(calc.getTotalBillBeforeTip()));
+        System.out.println("Tip percentage: " + calc.getTipPercentage() + "%");
+        System.out.println("Total tip: " + (formatter.format(calc.totalBill() - calc.getTotalBillBeforeTip())));
+        System.out.println("Total bill with tip: " + formatter.format(calc.totalBill()));
+        System.out.println("Per person cost before tip: " + (formatter.format(calc.perPersonCostBeforeTip())));
+        System.out.println("Tip per person: " + formatter.format((calc.perPersonTipAmount())));
+        System.out.println("Total cost per person: " + formatter.format(calc.perPersonTotalCost()));
+    }
+}
